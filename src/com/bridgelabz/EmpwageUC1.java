eb8be036c5f442fd50e257a3372d1df11d2316ed
swap123 <misalswapnil77@gmail.com>
2022-03-02 13:06:50 +0530

    Welcome to Employee Wages

--- src/com/bridgelabz/EmpwageUC1.java
+++ src/com/bridgelabz/EmpwageUC1.java
@@ -2,11 +2,13 @@ package com.bridgelabz;
 
 public class EmpwageUC1 {
 
+
     public static void main(String[] args) {
-        int P = 1;
+        System.out.println("Attendance");
+        int Attendance = 1;
         double empcheck = Math.floor(Math.random() * 10) % 2;
-        if (empcheck == P)
-            System.out.println("Employee is Love");
+        if (empcheck == Attendance)
+            System.out.println("Employee is Present");
         else
             System.out.println("Employee is Absent");
     }
